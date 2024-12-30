7a46f3279a13c3cc3435bfb5be94b3d2e9ee4da6
Ali-Ahmed-Alsaeedi <aliahmedalsaeedi218@gmail.com>
2024-12-30 10:23:18 -1000

    add fileds to note bottom sheet

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constans.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({Key? key}) : super(key: key);
-  // String? hintText;
+  const CustomTextField({Key? key, required this.hintText, this.maxLines = 1})
+      : super(key: key);
+
+  final String hintText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hintText,
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
